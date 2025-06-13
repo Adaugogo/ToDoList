79fe367034948e466f19e35226f4431055d3e06a
Adaugo Ukaumunna <106121642+Adaugogo@users.noreply.github.com>
2025-06-13 07:06:23 +0100

    my basic keeper app

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+
+function Header() {
+  return (
+    <header>
+      <h1><LibraryBooksIcon> </LibraryBooksIcon> Keeper App</h1>
+    </header>
+  );
+}
+
+export default Header;
