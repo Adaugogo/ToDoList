6d095ce9c18807a68bc92debfe8fe68366588be4
Adaugo Ukaumunna <106121642+Adaugogo@users.noreply.github.com>
2025-06-15 19:34:00 +0100

    trying to add some styling

--- src/header.jsx
+++ src/header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+
 
 function Header() {
   return (
-    <header>
-      <h1><LibraryBooksIcon> </LibraryBooksIcon> Keeper App</h1>
+    <header class='d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom'>
+      <h1>
+        <LibraryBooksIcon> </LibraryBooksIcon> Keeper App
+      </h1>
     </header>
   );
 }
